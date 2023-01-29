6e56715e4f48c504c5c5cc82f70006db950b46df
hpweisberg <hpweisberg@gmail.com>
2023-01-29 00:46:52 -0800

    add comment for google tag when ready

--- src/index.js
+++ src/index.js
@@ -14,4 +14,14 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-SKN6CKHQBV"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-SKN6CKHQBV');
+// </script>
+
 reportWebVitals();
