452849a99bef908da6b32e44e08f27eccd16eb2a
hpweisberg <hpweisberg@gmail.com>
2023-01-29 23:56:54 -0800

    add connect html

--- src/Pages/Connect/Connect.jsx
+++ src/Pages/Connect/Connect.jsx
@@ -5,7 +5,22 @@ const Connect = () => {
   return ( 
     <>
     
+      <h1 className={styles.Connect}>Connect</h1>
+      <a href="https://github.com/hpweisberg"><div className='github'></div></a>
+      <a href="https://www.linkedin.com/in/harrisonweisberg/"><div className='linkedIn'></div></a>
+      <a href="https://twitter.com/Harrison_PW"><div className='twitter'></div></a>
+      <div className='resume'></div>
+    
+      <form action="POST" autoComplete='off'>
+        <label htmlFor="name-input">Name</label>
+        <input type="text" name="name" id='name-input'></input>
+        <label htmlFor="email-input">Email</label>
+        <input type="text" name="email" id='email-input'></input>
+        <label htmlFor="message-input">Message</label>
+        <input type="text" name="message" id='message-input'></input>
 
+        <button className='send-message-btn' type='submit'>Send</button>
+      </form>
     </>
   )
 }
