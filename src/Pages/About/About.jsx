303323edbce92b43ce8fa8f5eefab076c73b6760
hpweisberg <hpweisberg@gmail.com>
2023-01-29 23:56:37 -0800

    add about html

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,7 +5,11 @@ import styles from './About.module.css'
 const About = () => {
   return ( 
     <>
+    <div className='bg-img'></div>
+      <h1 className={styles.About}>About</h1>
 
+      <p>I’m a full stack engineer who is completing a bootcamp at General Assembly. Making the career switch to software engineering was easy for since I grew up with a borderline obsession with tech. Many weekends were spent bouncing from Apple to the Verizon store. My thirst for the latest cutting edge tech led me to learning how to build software myself. I started coding with Harvard’s CS50 and I knew this was the path for me right away. Currently I’m working in Javascript, React, Typescript but I’m always looking to expand my skillset!</p>
+    
     </>
   )
 }
