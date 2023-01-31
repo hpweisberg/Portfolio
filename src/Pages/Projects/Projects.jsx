5fa042ca4d04775eac7d2f525d5062251f6a6b3d
hpweisberg <hpweisberg@gmail.com>
2023-01-31 00:00:18 -0800

    create projects data file

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -5,31 +5,33 @@ import styles from './Projects.module.css'
 const Projects = () => {
   return (
     <>
+    <div className="projectsContainer">
 
       <h3 className={styles.Projects}>Check out some of my projects</h3>
 
-      <div className='project-card'>
-        <div className='project-card-top-text'>
-          <div className='project-card-text'>
-            <a href="https://harrison-snake.netlify.app/"><h3 className='project-name'>MAMBA</h3></a>
-            <a href="https://github.com/hpweisberg/snake#readme"><div className='github-logo'></div></a>
+      <div className={styles.projectCard}>
+        <div className={styles.projectCardTopText}>
+          <div className={styles.projectCardText}>
+            <a href="https://harrison-snake.netlify.app/"><h3 className={styles.projectName}>MAMBA</h3></a>
+            <a href="https://github.com/hpweisberg/snake#readme"><div className={styles.githubLogo}></div></a>
           </div>
-          <p className='project-discription'>A take on the classic game of Snake. Players help NBA legend Kobe Bryant, aka, “Black Mamba” secure as many NBA Championship Trophies as possible without getting a technical foul!</p>
+          <p className={styles.projectDiscription}>A take on the classic game of Snake. Players help NBA legend Kobe Bryant, aka, “Black Mamba” secure as many NBA Championship Trophies as possible without getting a technical foul!</p>
         </div>
-        <a href="https://harrison-snake.netlify.app/"><div className='project-image-mobile'>{styles.Mamba}</div></a>
+        <a href="https://harrison-snake.netlify.app/"><div className={styles.projectImageMobile}>{styles.Mamba}</div></a>
       </div>
 
-      <div className='project-card'>
-        <div className='project-image-mobile'>{styles.Fly}</div>
-        <div className='project-card-top-text'>
-          <div className='project-card-text'>
-            <a href="https://flyy-harrisonw.fly.dev/"><h3 className='project-name'>FLYy</h3></a>
-            <a href="https://github.com/hpweisberg/FLYy#readme"><div className='github-logo'></div></a>
+      <div className={styles.projectCard}>
+        <div className={styles.projectImageMobile}>{styles.Fly}</div>
+        <div className={styles.projectCardTopText}>
+          <div className={styles.projectCardText}>
+            <a href="https://flyy-harrisonw.fly.dev/"><h3 className={styles.projectName}>FLYy</h3></a>
+            <a href="https://github.com/hpweisberg/FLYy#readme"><div className={styles.githubLogo}></div></a>
           </div>
-          <p className='project-discription'>A take on the classic game of Snake. Players help NBA legend Kobe Bryant, aka, “Black Mamba” secure as many NBA Championship Trophies as possible without getting a technical foul!</p>
+          <p className={styles.projectDiscription}>A take on the classic game of Snake. Players help NBA legend Kobe Bryant, aka, “Black Mamba” secure as many NBA Championship Trophies as possible without getting a technical foul!</p>
         </div>
       </div>
 
+    </div>
     </>
   )
 }
---
+++ src/data/projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    name: 'MAMBA',
+    description: 'A take on the classic game of Snake. Players help NBA legend Kobe Bryant, aka, “Black Mamba” secure as many NBA Championship Trophies as possible without getting a technical foul!',
+    screenshot: URL('../../')
+  },
+  {
+    name: 'FLYy',
+    description: `The flight tracker for you and your friends! Add your own flights and link up with your friends and family so you can stay up to date and upcoming trip. Check out your friend's journey posts to see what crazy adventures they've gotten up to. Or perhaps use the myJourney section to know when your family has landed safe and sound after each flight.`,
+    screenshot: URL('../../')
+  }
+]
+
+export default projects
