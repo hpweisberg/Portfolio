891c1d61385d5bb4425c1e41039d3441ee7dfcd5
hpweisberg <hpweisberg@gmail.com>
2023-02-19 14:06:56 -0800

    adjust soical links

--- src/Pages/Connect/Connect.jsx
+++ src/Pages/Connect/Connect.jsx
@@ -1,4 +1,8 @@
 import styles from './Connect.module.css'
+import github from '../../assets/githubMark.png'
+import linkedIn from '../../assets/linkedIn.png'
+import twitter from '../../assets/twitter.png'
+import resume from '../../assets/resume.png'
 
 
 const Connect = () => {
@@ -6,29 +10,32 @@ const Connect = () => {
 
   return (
     <>
-    {/* <Nav /> */}
-      <div className={styles.connecContainer}>
-        <section className={styles.content}>
-          <h1 className={styles.connect}>Connect</h1>
-          <div className={styles.socialLinks}>
-            <a href="https://github.com/hpweisberg" className={styles.github}><div className={styles.github}></div></a>
-            <a href="https://www.linkedin.com/in/harrisonweisberg/" className={styles.linkedIn}><div className={styles.linkedIn}></div></a>
-            <a href="https://twitter.com/Harrison_PW" className={styles.twitter}><div className={styles.twitter}></div></a>
-            {/* Fix download */}
-            <a href="../../../public/harrisonWeisbergResume.pdf" download="harrisonWeisbergResume.pdf" className={styles.resume}><div className={styles.resume}></div></a>
-          </div>
-        </section>
+      <div className={styles.connectContainer}>
+        <div className={styles.heroImg}>
+        </div>
+        <div className={styles.connectTextContainer}>
+          <section className={styles.connect}>
+            <h1>Let's chat</h1>
+            <div className={styles.socialLinks}>
+              <a href="https://github.com/hpweisberg" className={styles.github}><img src={github} alt="" /></a>
+              <a href="https://www.linkedin.com/in/harrisonweisberg/" className={styles.linkedIn}><img src={linkedIn} alt="" /></a>
+              <a href="https://twitter.com/Harrison_PW" className={styles.twitter}><img src={twitter} alt="" /></a>
+              {/* Fix download */}
+              <a href="../../../public/harrisonWeisbergResume.pdf" download="harrisonWeisbergResume.pdf" className={styles.resume}><img src={resume} alt="" /></a>
+            </div>
+          </section>
 
-        <form action="POST" autoComplete='off'>
-          <label htmlFor="nameInput">Name</label>
-          <input type="text" name="name" id='nameInput'></input>
-          <label htmlFor="emailInput">Email</label>
-          <input type="text" name="email" id='emailInput'></input>
-          <label htmlFor="messageInput">Message</label>
-          <input type="text" name="message" id='messageInput'></input>
+          <form action="POST" autoComplete='off'>
+            <label htmlFor="nameInput">Name</label>
+            <input type="text" name="name" id='nameInput'></input>
+            <label htmlFor="emailInput">Email</label>
+            <input type="text" name="email" id='emailInput'></input>
+            <label htmlFor="messageInput">Message</label>
+            <input type="text" name="message" id='messageInput'></input>
 
-          <button className={styles.sendMessageBtn} type='submit'>Send</button>
-        </form>
+            <button className={styles.sendMessageBtn} type='submit'>Send</button>
+          </form>
+        </div>
       </div>
     </>
   )
