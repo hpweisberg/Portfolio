a2c26d269be6b772ff09780c2e58ed998be6ba13
hpweisberg <hpweisberg@gmail.com>
2023-02-20 10:31:03 -0800

    project preview

--- src/data/projects.js
+++ src/data/projects.js
@@ -8,15 +8,16 @@ const myProjects = [
   },
   {
     name: 'FLYy',
-    description: `The flight tracker for you and your friends! Add your own flights and link up with your friends and family so you can stay up to date and upcoming trip. Check out your friend's journey posts to see what crazy adventures they've gotten up to. Or perhaps use the myJourney section to know when your family has landed safe and sound after each flight.`,
+    description: `The flight tracker for you and your friends! Add your own flights and link up with your friends and family so you can stay up to date and upcoming trip. 
+    Check out your friend's journey posts or perhaps use the myJourney section to know when your family has landed safe and sound after each flight.`,
     screenshot: '/flyy.jpg',
     repoLink: 'https://github.com/hpweisberg/FLYy',
     deploymentLink: 'https://flyy-harrisonw.fly.dev/',
   },
   {
     name: 'CAST',
-    description: `A better way for Casting Directors to get paired up with production talent. This app was born out of a desire to optimize a very manual casting process. Instead of sifting through resumes one by one, Cast allows Talent to setup a profile and have Casting Directors reach out to them. It's more simple, streamined, and modern.`,
-    screenshot: '/cast.png',
+    description: `A better way for Casting Directors to get paired up with production talent. This app was born out of a desire to optimize a very manual casting process. Instead of sifting through resumes one by one, Cast allows Talent to setup a profile and have Casting Directors reach out to them. Its more simple, streamined, and modern.`,
+    screenshot: '/castDesktop.png',
     repoLink: 'https://github.com/hpweisberg/cast-front-end',
     deploymentLink: 'https://castapp.netlify.app/',
   },
