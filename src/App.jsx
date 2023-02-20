9adc5f9ca03b0561900d7a721886c0cf02261264
hpweisberg <hpweisberg@gmail.com>
2023-02-20 12:27:58 -0800

    remove project details

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Landing from './Pages/Landing/Landing';
 import About from './Pages/About/About';
 import Projects from './Pages/Projects/Projects';
 import Connect from './Pages/Connect/Connect';
-import ProjectDetails from './Pages/ProjectDetails';
 // import projects from './data/projects';
 
 function App() {
@@ -20,7 +19,6 @@ function App() {
           <Route path='/' element={<Landing />} />
           <Route path='/About' element={<About />} />
           <Route path='/Projects' element={<Projects />} />
-          <Route path='/Projects/:ProjectDetails' element={<ProjectDetails />} />
           <Route path='/Connect' element={<Connect />} />
         </Routes>
       </>
