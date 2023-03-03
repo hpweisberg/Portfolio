94afaafe75e15be235ba0c029f33e9e293386e46
hpweisberg <hpweisberg@gmail.com>
2023-03-02 18:13:13 -0800

    add on button click to trigger animation for mobile

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -6,11 +6,31 @@ import meImg from '../../assets/hero-harrison.png'
 import mobileBackground from '../../assets/hero-image-background-crop.png'
 import mobileSand from '../../assets/hero-image-sand-crop.png'
 import mobileMe from '../../assets/hero-image-me-crop.png'
-import { NavLink } from 'react-router-dom'
 
+import { NavLink } from 'react-router-dom'
+import { useRef } from 'react'
 
 
 const Landing = ({ onButtonClick }) => {
+
+  const animateMe = useRef(null)
+  const animateSand = useRef(null)
+
+  function handleImgClick(){
+    console.log('hihi')
+    animateMe.current.classList.remove(`${styles.animateMeClass}`)
+    animateSand.current.classList.remove(`${styles.animateSandClass}`)
+    void animateMe.current.offsetWidth
+    void animateSand.current.offsetWidth
+    animateMe.current.classList.add(`${styles.animateMeClass}`)
+    animateSand.current.classList.add(`${styles.animateSandClass}`)
+  }
+
+  // function handleImgClick() {
+  //   setPlayAnimation(true)
+  //   console.log('button cliucked')
+  //   // setPlayAnimation(false)
+  // }
   return (
     <>
       <div className={styles.landingContainer}>
@@ -26,8 +46,8 @@ const Landing = ({ onButtonClick }) => {
           <div className={styles.mobileImgContainer}>
 
             <img src={mobileBackground} alt="Harrison sliding on sand" className={styles.mobileImg} />
-            <img src={mobileSand} alt="Harrison sliding on sand" className={styles.mobileImg} />
-            <img src={mobileMe} alt="Harrison sliding on sand" className={styles.mobileImg} />
+            <img src={mobileSand} alt="Harrison sliding on sand" className={styles.mobileImg} ref={animateSand} />
+            <img src={mobileMe} alt="Harrison sliding on sand" className={`${styles.mobileImg} ${styles.animateMeClass}`} onClick={handleImgClick} onLoad={handleImgClick} ref={animateMe}/>
           </div>
           <h1>
             <span className={styles.subNameSpan}>engineer, designer, creator extraordinaire.</span> Leveraging my years of experience in PR & brand management, I bring a unique perspective to app development. </h1>
